02011abae8f9ae350cf0de7e2d175bc57292f7bc
gaburinchyobear1 <shrck12345@gmail.com>
2024-04-02 23:17:00 +0900

    test1

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,129 @@
+﻿//과제물 프로젝트명: Lecture04_HW
+//idle : 화면색 검정색으로 clear
+//마우스 오른쪽 KeyDown하면 : 빨간색
+//마우스 오른쪽 KeyUp하면 : 원상복구
+//마우스 왼쪽 KeyDown하면 : 녹색
+//마우스 왼쪽 KeyUp하면 : 원상복구
+//마우스 오른쪽 KeyDown하고 드래그 중이면 : 파랑색
+//마우스 왼쪽 KeyDown하고 드래그 중이면 : 마젠타색
+
+//https://www.glfw.org/
+#pragma comment(lib, "Opengl32.lib")
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void errorCallback(int error, const char* description)
+{
+    std::cerr << "GLFW Error: " << description << std::endl;
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
+{
+
+    //while (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE){    
+
+
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+    {
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    }
+    else if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_RELEASE)
+    {
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    }
+    else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+        glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+    }
+    else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+    {
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+void cursorPositionCallback(GLFWwindow* window, double xpos, double ypos)
+{
+    int leftButtonState = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+    int rightButtonState = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT);
+
+    if (leftButtonState == GLFW_PRESS && rightButtonState == GLFW_RELEASE)
+    {
+        glClearColor(0.0f, 0.0f, 1.0f, 1.0f); // Blue color
+    }
+    else if (leftButtonState == GLFW_RELEASE && rightButtonState == GLFW_PRESS)
+    {
+        glClearColor(1.0f, 0.0f, 1.0f, 1.0f); // Magenta color
+    }
+    else
+    {
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Black color
+    }
+}
+
+
+
+
+
+
+
+
+
+int main(void)
+{
+    if (!glfwInit())
+        return -1;
+
+    GLFWwindow* window;
+    window = glfwCreateWindow(1280, 768, "MuSoeunEngine", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetErrorCallback(errorCallback);
+    glfwSetKeyCallback(window, keyCallback);
+    glfwSetMouseButtonCallback(window, mouseButtonCallback);
+    glfwSetCursorPosCallback(window, cursorPositionCallback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
